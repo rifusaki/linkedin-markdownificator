bfb5111e57220c70c7f6554e054cadf03fb6808e
rifusaki <46846373+rifusaki@users.noreply.github.com>
2023-10-16 15:06:03 -0500

    added session cookie support

--- main.py
+++ main.py
@@ -3,17 +3,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
+import pathlib
 import os
 
 load_dotenv()
-driver = webdriver.Chrome()
 
+options = Options()
+options.add_argument(f"user-data-dir={pathlib.Path().absolute()}\\selenium")
+driver = webdriver.Chrome(options=options)
 driver.get("https://co.linkedin.com/")
-driver.implicitly_wait(2)
+driver.implicitly_wait(5)
 
-driver.find_element(by=By.ID, value="session_key").send_keys(os.getenv("MAIL"))
-password = driver.find_element(by=By.ID, value="session_password")
-password.send_keys(os.getenv("PASSWORD"), Keys.ENTER)
-driver.implicitly_wait(30)
+if "feed" not in driver.title.lower():
+    print("login required")
+    driver.find_element(by=By.ID, value="session_key").send_keys(os.getenv("MAIL"))
+    password = driver.find_element(by=By.ID, value="session_password")
+    password.send_keys(os.getenv("PASSWORD"), Keys.ENTER)
+    driver.implicitly_wait(30)
+elif "feed" in driver.title.lower():
+    print("session cookie preserved")
 
+driver.implicitly_wait(5)
 driver.quit()
